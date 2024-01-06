d55d35ae544323bb40f9042cc0570d290bece114
TheRealPlagiariser <shujaatkhanumarkhan@gmail.com>
2024-01-07 01:25:54 +0400

    added

--- src/app/services/case-service.ts
+++ src/app/services/case-service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+// To access the API URL
+const apiUrl = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class CaseService {
-  private baseUrl = 'http://localhost:3000/api/cases';
 
   constructor(private http: HttpClient) { }
 
   listCases(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/list`);
+    return this.http.get(`${apiUrl}/cases/list`);
   }
 }
